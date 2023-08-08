85f6de5e77f2c35f0c1209b39632542cacaebbfb
Bastouwln <wathicyiscrazyomg@gmail.com>
2023-08-08 17:13:35 -0300

    Arquivo aula 1. Introdução ao javascript.

---
+++ aula_01/aula2.js
@@ -0,0 +1,148 @@
+//const, bool, var, let
+
+
+/*
+var pessoa = {
+    nome: "Junior", idade: "33",
+    
+};
+
+console.log(pessoa.nome);
+
+
+function main(){
+    var pessoa = {
+        nome: "Junior", idade: "33",
+        
+    };
+    
+    console.log(pessoa.nome);
+}
+
+main();
+console.log(typeof pessoa.idade);
+
+*/
+
+
+
+
+
+
+// if-else
+
+
+/* var situacao = true;
+
+if(situacao){
+    console.log("Você está ativo. " + "💕");
+}
+else{
+    console.log("Você está inativo. " + "😎");
+}
+
+
+console.log("Exemplo variável: ", situacao)
+
+
+// == se é um valor igual ao outro
+// === se é um valor igual ao outro e do mesmo tipo
+
+*/
+
+
+
+
+/*
+const lista = ["Douglas", "Laízia", "Otávio", "Diego", "João"];
+
+const tamanhoLista = lista.length - 1;
+
+//console.log(lista);
+//console.log(lista[1]);
+
+console.log("Tamanho da lista: " + tamanhoLista);
+
+for(let i=0; i<=tamanhoLista; i++){
+if(i == 4){
+    console.log("Aluno(a): "+lista[i]+ " Reprovou")
+}else{
+console.log("Aluno(a): " + lista[i])}
+}
+
+console.log("-----while----")
+
+var i2 = 0;
+while(i2 <= tamanhoLista){
+    if(i2 == 4){
+        console.log("Aluno(a): "+lista[i2]+ " Reprovou")
+    }else{
+    console.log("Aluno(a): " + lista[i2])}
+    i2++
+}
+console.log("-- - forEach - --")
+
+lista.forEach((value, index)=>{
+    if(value == "Douglas"){
+        console.log(index + " - " + value + " - Reprovado")
+    }
+    else{
+console.log(index + " - " + value);
+    }
+});
+
+
+*/
+/*
+const valorDeEntrada = 8;
+
+switch (valorDeEntrada) {
+    case 1:
+        console.log("O valor é 1");
+        break;
+        case 2:
+            console.log("O valor é 2");
+            break;
+            case 3:
+                console.log("O valor é 3");
+                break;
+
+    default:
+        console.log("quebrô");
+        break;
+}
+
+*/
+
+
+
+
+
+
+//object
+
+/*
+const lista = ["Douglas", "Laízia", "Otávio", "Diego", "João"];
+
+console.log("-- - object - --")
+
+const alunos = [
+    { nome: "João", idade:16 },
+    { nome: "Douglas", idade:16 },
+    { nome: "Laízia", idade:17 },
+    { nome: "Otávio", idade:16 },
+    { nome: "Diego", idade:16 },
+    "Olá mundo, eu vim para estragar!",
+
+];
+
+alunos.forEach((value) =>{
+    if(typeof value == "object"){
+    console.log(value.nome, " ", value.idade);
+    }
+    else{
+        console.log(value);
+    }
+})
+
+*/
