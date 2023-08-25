f35f1dc24b5d71ef7fa82afdee89b77c1f943728
Bastouwln <wathicyiscrazyomg@gmail.com>
2023-08-25 17:14:51 -0300

    atualizações trabalho

--- trabalho_01/js/script.js
+++ trabalho_01/js/script.js
@@ -1,6 +1,26 @@
-function getMenu(){
-var chkMenu = document.getElementById(chkMenu)
+function changeMarisa(){
+document.getElementById("title").innerHTML = "Marisa - THW"
+}
+function changeReimu(){
+document.getElementById("title").innerHTML = "Reimu - THW"
+}
+function changeRemilia(){
+document.getElementById("title").innerHTML = "Remilia - THW"
 }
 
+reimu = document.getElementById("reimu")
+marisa = document.getElementById("marisa")
+remilia = document.getElementById("remilia")
 
-getMenu()
+console.log(reimu)
+
+
+if(reimu = true){
+    changeReimu()
+}
+else if(marisa = true){
+    changeMarisa()
+}
+else if(remilia = true){
+    changeRemilia()
+}
