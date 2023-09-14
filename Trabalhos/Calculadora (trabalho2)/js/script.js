905664a24e067d16c98615cd1c275a29d1d82c64
Bastouwln <wathicyiscrazyomg@gmail.com>
2023-09-14 14:37:21 -0300

    Atualizações trabalho - 14/09/2023

--- Trabalhos/Calculadora (trabalho2)/js/script.js
+++ Trabalhos/Calculadora (trabalho2)/js/script.js
@@ -1,26 +1,29 @@
 const resposta = window.document.getElementsByClassName("catch")[0];
-const resposta2 = window.document.getElementsByClassName("child")[0];
 var temp = "";
-
+var type1;
+var type2;
+//function a (op){
 function cuca(op){
     var valor1 = document.getElementById("valor1").value;
     var valor2 = document.getElementById("valor2").value;
-    valor1 = Number(valor1);
-    valor2 = Number(valor2);
-
 
     var a = window.document.createElement("p");
     a.setAttribute("class","resultado3");
     resposta.appendChild(a);
 
+    valor1 = Number(valor1);
+    valor2 = Number(valor2);
+    console.log(valor1, valor2);
+
+
     if(op != temp){
         clear();
     }
-    var valor3
-    console.log(temp)
-    if(op === '+'){
+    var valor3;
+    if(op == '+'){
+
         valor3 = valor1 + valor2;
-        if(valor1 && valor2 == 0){
+        if(valor1 == 0 && valor2 == 0){
             a.innerHTML = "Ambos os valores não detectados";
         }else if(valor1 == 0){
             a.innerHTML = "Coloque o primeiro valor.";
@@ -35,7 +38,7 @@ function cuca(op){
         temp = "+"
     }
     if(op =='-'){
-        if(valor1 && valor2 == 0){
+        if(valor1 == 0 && valor2 == 0){
             a.innerHTML = "Ambos os valores não detectados";
         }else if(valor1 == 0){
             a.innerHTML = "Coloque o primeiro valor.";
@@ -52,7 +55,7 @@ function cuca(op){
         temp = "-"
     }
     if(op =='*'){
-        if(valor1 && valor2 == 0){
+        if(valor1 == 0 && valor2 == 0){
             a.innerHTML = "Ambos os valores não detectados";
         }else if(valor1 == 0){
             a.innerHTML = "Coloque o primeiro valor.";
@@ -65,9 +68,16 @@ function cuca(op){
 
         a.innerHTML = "Seu valor da sua multiplicação é : " + valor3;
         }
+        temp = "*"
     }
     if(op =='/'){
-        if(valor1 && valor2 == 0){
+        if(valor1 == 0 && valor2 == 1){
+            a.innerHTML = "Não se pode dividir 0 por 1.";
+        }
+        else if(valor1 == 1 && valor2 == 0){
+            a.innerHTML = "Não se pode dividir 1 por 0.";
+        }
+        else if(valor1 == 0 && valor2 == 0){
             a.innerHTML = "Ambos os valores não detectados";
         }else if(valor1 == 0){
             a.innerHTML = "Coloque o primeiro valor.";
@@ -80,8 +90,11 @@ function cuca(op){
 
         a.innerHTML = "Seu valor da sua divisão é : " + valor3;
         }
+        temp = "/"
     }
 }
 function clear(){
     resposta.innerHTML = "";
 }
+
+//}
